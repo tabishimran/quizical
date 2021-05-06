93e3ede60ac948709ebac2416f5a0af4831d7098
tabishimran <tabish.imran96@gmail.com>
2021-05-06 12:26:03 +0530

    route imports

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -1,11 +1,15 @@
 const Hapi = require('@hapi/hapi');
+const routes = require('./routes/index');
+
 
 const init = async () => {
-
     const server = Hapi.server({
         port: 4000,
         host: 'localhost'
     });
+
+    server.route(routes);
+
 
     await server.start();
     console.log('Server running on %s', server.info.uri);
