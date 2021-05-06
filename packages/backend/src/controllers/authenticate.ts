652a2e6b01f01afc6bb82a1a4a91bec3e97fe130
tabishimran <tabish.imran96@gmail.com>
2021-05-06 14:37:43 +0530

    adds authentication controller

---
+++ packages/backend/src/controllers/authenticate.ts
@@ -0,0 +1,49 @@
+import fetch,{Headers} from 'node-fetch'
+import {Request,ResponseToolkit} from 'hapi';
+
+module.exports = async function authenticate(request: Request ,reply : ResponseToolkit){
+    // check if code was returned
+    var code = request.query.code;
+    if(!code) return reply.response(request.query.error).code(401);
+
+    // generate tokens
+    var tokens = await getTokens(code);
+    if(tokens.error) return reply.response(tokens.error_description).code(401);
+    
+    // generate userdata 
+    var userData = await getUserInfo(tokens.access_token);
+    if(userData.error) return reply.response(userData.error).code(401);
+
+    // create account if it doesn't exist
+    var userInfo = createUser(userData);
+
+    // create and set cookies 
+
+    return userInfo;
+}
+
+
+async function getTokens(code){
+    const params = new URLSearchParams();  
+    params.append('grant_type','authorization_code');
+    params.append('code',code);
+    params.append('redirect_uri','https://quizical.tabishimran.com/api/authenticate');
+    params.append('client_id',process.env.spotify_client_id);
+    params.append('client_secret',process.env.spotify_client_secret)
+    const response = await fetch('https://accounts.spotify.com/api/token', {method:"POST","body":params});
+    const data = await response.json();
+    return data;
+}
+
+async function getUserInfo(access_token){
+    var headerObj={Authorization:'Bearer '+access_token}
+    const headers  = new Headers(headerObj);
+    const response = await fetch('https://api.spotify.com/v1/me',{method:"GET",headers:headers});
+    const data = await response.json();
+    return data;
+}
+
+async function createUser(userData){
+    return userData;
+}
+
