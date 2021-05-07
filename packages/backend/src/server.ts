a9e2a6679c11335189bb06d395d3bc52c48cc458
tabishimran <tabish.imran96@gmail.com>
2021-05-07 15:35:43 +0530

    updates code to use the new config file

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -1,16 +1,26 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes/index');
-
+const configuration = require('./config/index');
 
 const init = async () => {
+    // initialize server
     const server = Hapi.server({
-        port: 4000,
-        host: '0.0.0.0'
+        port: configuration.server.port,
+        host: configuration.server.host
     });
-    server.realm.modifiers.route.prefix = '/api';
+
+    // setup route prefix
+    server.realm.modifiers.route.prefix = configuration.server.route_prefix;
+
+    // setup auth strategy
+    await server.register(require('@hapi/cookie'));
+    server.auth.strategy('session','cookie',configuration.auth);
+    server.auth.default('session');
+
+    // setup server routes
     server.route(routes);
 
-
+    // start server
     await server.start();
     console.log('Server running on %s', server.info.uri);
 };
