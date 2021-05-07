b39241978a1d48189353a414b08584207141b6b5
tabishimran <tabish.imran96@gmail.com>
2021-05-07 20:14:18 +0530

    updates session object

--- packages/backend/src/controllers/search.ts
+++ packages/backend/src/controllers/search.ts
@@ -1,4 +1,4 @@
 module.exports = async function search(request,reply){
-    const session = request.auth.credentials
+    const session = request.state;
     return '<h1>search</h1>'+JSON.stringify(session);
 }
--- packages/backend/src/controllers/wordcloud.ts
+++ packages/backend/src/controllers/wordcloud.ts
@@ -1,4 +1,4 @@
 module.exports = async function wordcloud(request,reply){
-    const session = request.auth.credentials;
+    const session = request.state;
     return '<h1>wordcloud</h1>'+JSON.stringify(session);
 }
