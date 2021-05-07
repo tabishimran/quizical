8d31fbb7d482b606242d959308358f22a4104322
tabishimran <tabish.imran96@gmail.com>
2021-05-07 20:14:33 +0530

    adds user model

---
+++ packages/backend/src/models/user.ts
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+var userSchema = new Schema({
+    name:String,
+    country:String,
+    email:String,
+    uri:{ type : String , unique : true, required : true, dropDups: true },
+    product:String,
+    tokens:Object
+});
+
+module.exports = mongoose.model('Users',userSchema);
