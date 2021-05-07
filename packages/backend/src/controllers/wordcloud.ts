254c8219339948c9ec1543c2f3d28470aa354a9a
tabishimran <tabish.imran96@gmail.com>
2021-05-08 01:11:52 +0530

    implements wordcloud controller

--- packages/backend/src/controllers/wordcloud.ts
+++ packages/backend/src/controllers/wordcloud.ts
@@ -1,11 +1,20 @@
 const User = require('../models/user');
 import fetch,{Headers} from 'node-fetch'
-const request = require('../utils/authenticatedRequest');
+const authenticatedRequest = require('../utils/authenticatedRequest');
 
 
 module.exports = async function wordcloud(request,reply){
     const session = request.state;
     const topArtistsURL = "https://api.spotify.com/v1/me/following?type=artist&limit=50";
-    const topArtists = await request(topArtistsURL,session,'GET');
+    const topArtists = await authenticatedRequest(topArtistsURL,session,'GET');
+    const wordCloud = generateWordCloud(topArtists);
     return '<h1>wordcloud</h1>'+JSON.stringify(topArtists);
+}
+
+async function generateWordCloud(topArtists){
+    var artists = topArtists.artists.items;
+    var wordCloud =  artists.map(function(artist){
+        return {value:artist.name,count:artist.popularity}
+    });
+    return wordCloud;
 }
