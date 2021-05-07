0837ed8b05ee29a089521dc206f68be3ef5e45e9
tabishimran <tabish.imran96@gmail.com>
2021-05-08 00:36:50 +0530

    implemets top artists for wordcloud

--- packages/backend/src/controllers/wordcloud.ts
+++ packages/backend/src/controllers/wordcloud.ts
@@ -1,4 +1,11 @@
+const User = require('../models/user');
+import fetch,{Headers} from 'node-fetch'
+const request = require('../utils/authenticatedRequest');
+
+
 module.exports = async function wordcloud(request,reply){
     const session = request.state;
-    return '<h1>wordcloud</h1>'+JSON.stringify(session);
+    const topArtistsURL = "https://api.spotify.com/v1/me/following?type=artist&limit=50";
+    const topArtists = await request(topArtistsURL,session,'GET');
+    return '<h1>wordcloud</h1>'+JSON.stringify(topArtists);
 }
