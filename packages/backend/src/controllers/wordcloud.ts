a90e58be2257115cd3f7f50e585786aa5757a62f
tabishimran <tabish.imran96@gmail.com>
2021-05-08 01:26:26 +0530

    updates controller to return JSON output

--- packages/backend/src/controllers/wordcloud.ts
+++ packages/backend/src/controllers/wordcloud.ts
@@ -1,14 +1,15 @@
 const User = require('../models/user');
+import { ResponseToolkit } from 'hapi';
 import fetch,{Headers} from 'node-fetch'
 const authenticatedRequest = require('../utils/authenticatedRequest');
 
 
-module.exports = async function wordcloud(request,reply){
+module.exports = async function wordcloud(request,reply:ResponseToolkit){
     const session = request.state;
     const topArtistsURL = "https://api.spotify.com/v1/me/following?type=artist&limit=50";
     const topArtists = await authenticatedRequest(topArtistsURL,session,'GET');
-    const wordCloud = generateWordCloud(topArtists);
-    return '<h1>wordcloud</h1>'+JSON.stringify(topArtists);
+    const wordCloud = await generateWordCloud(topArtists);
+    return reply.response(wordCloud);
 }
 
 async function generateWordCloud(topArtists){
