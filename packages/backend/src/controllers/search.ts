16682ab1bdfe8f8fedcf15e3f6bce0be7221f3e8
Tabish Imran <tabish.imran96@gmail.com>
2021-05-08 14:38:12 +0530

    implements search controller

--- packages/backend/src/controllers/search.ts
+++ packages/backend/src/controllers/search.ts
@@ -1,4 +1,13 @@
+const authenticatedRequest = require('../utils/authenticatedRequest');
+
+
 module.exports = async function search(request,reply){
     const session = request.state;
-    return '<h1>search</h1>'+JSON.stringify(session);
+    const params = new URLSearchParams();
+    params.append('type','artist');
+    params.append('limit','10');
+    params.append('q',request.query.key);
+    const searchURL = "https://artist-explorer.glitch.me/spotify/search";
+    const data = authenticatedRequest(searchURL,session,{body:params})
+    return reply.response(data);
 }
