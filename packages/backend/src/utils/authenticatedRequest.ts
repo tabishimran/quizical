eb10f9f77b6066fa98eb79cb18da2c52655321cc
Tabish Imran <tabish.imran96@gmail.com>
2021-05-08 14:39:34 +0530

    restructuring

--- packages/backend/src/utils/authenticatedRequest.ts
+++ packages/backend/src/utils/authenticatedRequest.ts
@@ -3,18 +3,18 @@ import fetch,{Headers} from 'node-fetch'
 const _ = require('lodash');
 const User = require('../models/user');
 
-async function request(url,session,method='GET'){
+async function request(url,session,options){
     const uri = session.sid.id;
     const user = await User.findOne({"uri":uri});
     var headers = await generateHeader(user.tokens);
-    const response = await fetch(url,{method:method,headers:headers});
-    const dataObj = await response.json();
+    options.headers = headers;
+    const dataObj = await sendRequest(url,options);
     if(_.get(dataObj,'error.message')=="The access token expired"){
             console.log("token expired, refreshing");
             var tokens = await refreshTokens(user);
             headers = await generateHeader(tokens);
-            const response = await fetch(url,{method:method,headers:headers});
-            const dataObj = await response.json();
+            options.headers = headers;
+            const dataObj = await sendRequest(url,options);
     }
     return dataObj;
 }
@@ -27,8 +27,7 @@ async function refreshTokens(user){
     let encodedHeader = Buffer.from(plainTextHeader).toString('base64');
     const headerObj={Authorization:"Basic "+encodedHeader}
     const headers = new Headers(headerObj);
-    const response = await fetch("https://accounts.spotify.com/api/token",{method:"POST",body:params,headers:headers})
-    const data = await response.json();
+    const data = await sendRequest("https://accounts.spotify.com/api/token",{method:"POST",body:params,headers:headers});
     const token = data.access_token;
     const tokenObj = user.tokens;
     tokenObj.access_token = token;
@@ -40,6 +39,13 @@ async function generateHeader(tokens){
     var headerObj={Authorization:'Bearer '+tokens.access_token}
     const headers  = new Headers(headerObj);
     return headers;
+}
+
+
+async function sendRequest(url:string,options:object) {
+    const response = await fetch(url,options);
+    const data = await response.json();
+    return data;
 }
 
 module.exports = request
