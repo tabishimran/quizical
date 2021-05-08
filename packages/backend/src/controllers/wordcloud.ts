b6e3ce5148082e0d5920cb7abe572a5ecfcf3fdc
Tabish Imran <tabish.imran96@gmail.com>
2021-05-08 14:41:12 +0530

    updates wordcloud controller

--- packages/backend/src/controllers/wordcloud.ts
+++ packages/backend/src/controllers/wordcloud.ts
@@ -1,13 +1,13 @@
 const User = require('../models/user');
 import { ResponseToolkit } from 'hapi';
-import fetch,{Headers} from 'node-fetch'
 const authenticatedRequest = require('../utils/authenticatedRequest');
 
 
 module.exports = async function wordcloud(request,reply:ResponseToolkit){
     const session = request.state;
     const topArtistsURL = "https://api.spotify.com/v1/me/following?type=artist&limit=50";
-    const topArtists = await authenticatedRequest(topArtistsURL,session,'GET');
+    const options = {method:'GET'}
+    const topArtists = await authenticatedRequest(topArtistsURL,session,options);
     const wordCloud = await generateWordCloud(topArtists);
     return reply.response(wordCloud);
 }
