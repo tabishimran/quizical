dac89ae5d77b9a85f2f6ccfdc3de011f638d55d2
Tabish Imran <tabish.imran96@gmail.com>
2021-05-09 23:59:53 +0530

    moves navbar to a separate component

---
+++ packages/frontend/src/components/NavBar.tsx
@@ -0,0 +1,22 @@
+import '../css/App.css'
+import { AppBar, IconButton, Toolbar, Typography } from '@material-ui/core';
+import React from 'react';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+function NavBar(){
+    return(
+        <AppBar position="absolute" style={{backgroundColor:"#191414"}}>
+        <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon style={{color:"white"}}/>
+          </IconButton>
+          <Typography variant="h3" style={{color:"white", fontSize:"1.5rem"}}>
+            Quizical
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    );
+}
+
+export default NavBar;
