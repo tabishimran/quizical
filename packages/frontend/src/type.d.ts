32a264c64635fc07ef964e4e165c299ff6dcce28
Tabish Imran <tabish.imran96@gmail.com>
2021-05-09 18:54:56 +0530

    updates tile type def

--- packages/frontend/src/type.d.ts
+++ packages/frontend/src/type.d.ts
@@ -3,3 +3,12 @@ declare global {
         onSpotifyWebPlaybackSDKReady:any;
     }
 }
+
+type tile={
+    img: string,
+    title: string,
+    popularity: number,
+    imgHeight:number,
+    imgWidth:number,
+    uri: string
+}
