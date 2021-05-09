313b68b151488f21fe9295726939c6f5c7e2908d
Tabish Imran <tabish.imran96@gmail.com>
2021-05-10 01:14:05 +0530

    returns artist results only if the artist has an image

--- packages/backend/src/controllers/search.ts
+++ packages/backend/src/controllers/search.ts
@@ -1,30 +1,32 @@
 const authenticatedRequest = require('../utils/authenticatedRequest');
 
 
-module.exports = async function search(request,reply){
+module.exports = async function search(request, reply) {
     const session = request.state;
     const params = new URLSearchParams();
-    params.append('type','artist');
-    params.append('limit','10');
-    params.append('q',request.query.key);
+    params.append('type', 'artist');
+    params.append('limit', '10');
+    params.append('q', request.query.key);
     const searchURL = new URL("https://api.spotify.com/v1/search")
     searchURL.search = params.toString()
-    const data = await authenticatedRequest(searchURL.toString(),session,{method:'GET'})
-    const searchResults = generateSearchResults(data.artists.items);
-    return reply.response(data);
+    const data = await authenticatedRequest(searchURL.toString(), session, { method: 'GET' })
+    const searchResults = generateSearchResults(data);
+    return reply.response(searchResults);
 }
 
-async function generateSearchResults(topArtists){
+async function generateSearchResults(topArtists) {
     var artists = topArtists.artists.items;
-    var searchResults =  artists.map(function(artist){
-        var image = artist.images[0];
-        return {
-            title:artist.name,
-            popularity:artist.popularity,
-            img:image.url,
-            imgHeight:image.height,
-            imgWidth:image.width,
-            uri:artist.uri
+    var searchResults = artists.map(function (artist) {
+        if (artist.images.length) {
+            var image = artist.images[0];
+            return {
+                title: artist.name,
+                popularity: artist.popularity,
+                img: image.url,
+                imgHeight: image.height,
+                imgWidth: image.width,
+                uri: artist.uri
+            }
         }
     });
     return searchResults;
