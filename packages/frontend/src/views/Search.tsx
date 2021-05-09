e987feb35ef1edd14bf469438acc16a154c4a2d1
Tabish Imran <tabish.imran96@gmail.com>
2021-05-09 18:56:26 +0530

    updates search view

--- packages/frontend/src/views/Search.tsx
+++ packages/frontend/src/views/Search.tsx
@@ -1,33 +1,32 @@
 import { Divider, Grid, Typography } from "@material-ui/core";
 import '../css/App.css';
-import WordCloud from '../components/WordCloud';
 import SearchBar from '../components/SearchBar';
+import ImageGrid from "../components/ImageGrid";
 
-function Search(){
-    return(
-        <div className="container">
-        <div className="view">
-          <Grid
-            container 
-            justify="center" 
-            alignItems="center" 
-            style={{textAlign:"center",height:'100%'}} 
-            direction="column"
-          >
-            <Grid item>
-              <Typography variant="h5" style={{color:"white",marginTop:'6rem'}} > Pick an artist</Typography>
-            </Grid>
-            <Grid item>
-              <SearchBar></SearchBar>
-            </Grid>
-            <Divider orientation="horizontal" style={{color:"white"}}></Divider>
-            <Grid item xs={12}>
-              <WordCloud></WordCloud>
-            </Grid>
+function Search() {
+  return (
+    <div className="container">
+      <div className="view">
+        <Grid
+          container
+          justify="center"
+          alignItems="center"
+          style={{ textAlign: "center" }}
+          direction="row"
+          wrap="wrap"
+        >
+          <Grid item>
+            <SearchBar></SearchBar>
           </Grid>
-          </div>
-      </div>  
-    );
+          {/* <div className="scrollable"> */}
+            <Grid item style={{overflowY:"auto",height:"1024px "}}>
+              <ImageGrid ></ImageGrid>
+            </Grid>
+          {/* </div> */}
+        </Grid>
+      </div>
+    </div>
+  );
 }
 
 export default Search;
