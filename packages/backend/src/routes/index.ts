e6a97062c727c90701d7baab414c945930e2a9ba
Tabish Imran <tabish.imran96@gmail.com>
2021-05-12 16:12:46 +0530

    fixes typo

--- packages/backend/src/routes/index.ts
+++ packages/backend/src/routes/index.ts
@@ -2,7 +2,7 @@ const loginRoute = require('./login');
 const authenticateRoute = require('./authenticate');
 const searchRoute = require('./search');
 const imageGridRoute = require('./grid');
-const quizRoutes = require('./quiz')
+const quizRoute = require('./quiz')
 
-module.exports= [].concat(loginRoute,authenticateRoute,searchRoute,imageGridRoute,quizRoutes);
+module.exports= [].concat(loginRoute,authenticateRoute,searchRoute,imageGridRoute,quizRoute);
 
