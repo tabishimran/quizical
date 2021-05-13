264d4d1a41d0be06d7e78067f70c9ea7ffc4bd86
Tabish Imran <tabish.imran96@gmail.com>
2021-05-13 18:09:15 +0530

    Implements player using react-sound

--- packages/frontend/src/components/Player.tsx
+++ packages/frontend/src/components/Player.tsx
@@ -1,4 +1,4 @@
-import { Card, CardActionArea, createStyles, Grid, IconButton, makeStyles } from "@material-ui/core";
+import { Card, CardActionArea, createStyles, Grid, IconButton, makeStyles, Theme, useTheme } from "@material-ui/core";
 import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
 import Forward10RoundedIcon from '@material-ui/icons/Forward10Rounded';
 import ReplayRoundedIcon from '@material-ui/icons/ReplayRounded';
@@ -8,118 +8,143 @@ import StarRoundedIcon from '@material-ui/icons/StarRounded';
 import StarBorderRoundedIcon from '@material-ui/icons/StarBorderRounded';
 import PauseCircleFilledIcon from '@material-ui/icons/PauseCircleFilled';
 import { useEffect, useState } from "react";
+import "../css/App.css";
+
+var Sound = require('react-sound').default;
+
+
 
 declare const window: any;
 
+interface playerProps {
+    audio: string
+}
 
 const useStyles = makeStyles((theme) =>
-  createStyles({
-    controls: {
-      marginLeft:'1rem',
-      marginRight:'1rem',
-      display: 'flex',
-      alignItems: 'center',
-      paddingLeft: theme.spacing(1),
-      paddingRight: theme.spacing(1),
-    },
-    icon: {
-      height: 48,
-      width: 48,
-    },
-  }),
+    createStyles({
+        controls: {
+            marginLeft: '1rem',
+            marginRight: '1rem',
+            display: 'flex',
+            alignItems: 'center',
+            paddingLeft: theme.spacing(1),
+            paddingRight: theme.spacing(1),
+        },
+        icon: {
+            height: 48,
+            width: 48,
+        },
+    }),
 );
 
-function Player(){
-
+function Player(props: playerProps) {
+    const theme = useTheme();
+    const audioURL = props.audio;
     const classes = useStyles();
-    const [sdkLoaded,setSDKLoaded] = useState(false);
-    const [playerReady,setPlayerReady] = useState(false);
-    const [deviceID,setDeviceId] = useState(null);
-    const [songPlaying,playSong] = useState(false);
+    const [sdkLoaded, setSDKLoaded] = useState(false);
+    const [playerReady, setPlayerReady] = useState(false);
+    const [deviceID, setDeviceId] = useState(null);
+    const [songPlaying, setSongPlaying] = useState(false);
+
+
 
     useEffect(() => {
         const scriptTag = document.createElement('script');
         scriptTag.src = 'https://sdk.scdn.co/spotify-player.js';
-        scriptTag.addEventListener('load',()=>setSDKLoaded(true));
+        scriptTag.addEventListener('load', () => setSDKLoaded(true));
         document.body.appendChild(scriptTag);
     }, [])
 
     useEffect(() => {
-        window.onSpotifyWebPlaybackSDKReady = function(){
+        window.onSpotifyWebPlaybackSDKReady = function () {
             const token = '';
             const player = new window.Spotify.Player({
-              name: 'Quizical',
-              getOAuthToken: (cb: (arg0: string) => void) => { cb(token); }
-            });            
+                name: 'Quizical',
+                getOAuthToken: (cb: (arg0: string) => void) => { cb(token); }
+            });
             // Error handling
-            player.addListener('initialization_error', ({ message }:{message:any}) => { console.error(message); });
-            player.addListener('authentication_error', ({ message }:{message:any}) => { console.error(message); });
-            player.addListener('account_error', ({ message }:{message:any}) => { console.error(message); });
-            player.addListener('playback_error', ({ message }:{message:any}) => { console.error(message); });
-          
+            player.addListener('initialization_error', ({ message }: { message: any }) => { console.error(message); });
+            player.addListener('authentication_error', ({ message }: { message: any }) => { console.error(message); });
+            player.addListener('account_error', ({ message }: { message: any }) => { console.error(message); });
+            player.addListener('playback_error', ({ message }: { message: any }) => { console.error(message); });
+
             // Playback status updates
             player.addListener('player_state_changed', (state: any) => { console.log(state); });
-                    
-            player.addListener('ready', ({ device_id }:{device_id:any}) => {
-              setDeviceId(device_id);
-              setPlayerReady(true);
-            });
-          
-            // Not Ready
-            player.addListener('not_ready', ({ device_id }:{device_id:any}) => {
-              console.log('Device ID has gone offline', device_id);
-              setDeviceId(null);
-              setPlayerReady(false);
-            });
-          
-            // Connect to the player!
-            console.log("SDK Loaded   : "+sdkLoaded);
-            console.log("Player Ready : "+playerReady);
-            player.connect();
-          };
-    },[])
 
-    function handlePlayButton(){
-       playSong(!songPlaying);
+            player.addListener('ready', ({ device_id }: { device_id: any }) => {
+                setDeviceId(device_id);
+                setPlayerReady(true);
+            });
+
+            // Not Ready
+            player.addListener('not_ready', ({ device_id }: { device_id: any }) => {
+                console.log('Device ID has gone offline', device_id);
+                setDeviceId(null);
+                setPlayerReady(false);
+            });
+
+            // Connect to the player!
+            console.log("SDK Loaded   : " + sdkLoaded);
+            console.log("Player Ready : " + playerReady);
+            player.connect();
+        };
+    }, [])
+
+    function playSong() {
+        setSongPlaying(true);
+        setTimeout(()=>{
+            pauseSong()
+        },10000)
     }
-    
-    return(
-            <Card>
-                <Grid container direction="row" justify="center" alignItems="center">
-                    <div className={classes.controls}>
+
+    function pauseSong() {
+        setSongPlaying(false);
+    }
+
+
+    return (
+        <Card className="playDock">
+            <Grid container direction="row" justify="center" alignItems="center">
+                <div className={classes.controls}>
                     <Grid item>
                         <IconButton>
-                            <StarRoundedIcon/>
-                        </IconButton>   
-                    </Grid>
-                    <Grid item>
-                        <IconButton>
-                            <ReplayRoundedIcon/>
-                        </IconButton>   
-                    </Grid>
-                    <Grid item>
-                        {songPlaying?
-                        <IconButton style={{color:"#1DB954"}} onClick={handlePlayButton}>
-                            <PauseCircleFilledIcon className={classes.icon} style={{color:"primary"}}/>
-                        </IconButton> 
-                        :
-                        <IconButton style={{color:"#1DB954"}} onClick={handlePlayButton}>
-                            <PlayCircleFilledIcon className={classes.icon} style={{color:"primary"}}/>
-                        </IconButton>}    
+                            <StarRoundedIcon />
+                        </IconButton>
                     </Grid>
                     <Grid item>
                         <IconButton>
-                            <Forward10RoundedIcon/>
-                        </IconButton>   
+                            <ReplayRoundedIcon />
+                        </IconButton>
+                    </Grid>
+                    <Grid item>
+                        <Sound
+                            url={audioURL}
+                            playStatus={songPlaying? Sound.status.PLAYING : Sound.status.PAUSED}
+                            playFromPosition={0 /* in milliseconds */}
+                            onFinishedPlaying={pauseSong}
+                        />
+                        {songPlaying ?
+                            <IconButton style={{ color: "#1DB954" }} onClick={pauseSong}>
+                                <PauseCircleFilledIcon className={classes.icon} style={{ color: "primary" }} />
+                            </IconButton>
+                            :
+                            <IconButton style={{ color: "#1DB954" }} onClick={playSong}>
+                                <PlayCircleFilledIcon className={classes.icon} style={{ color: "primary" }} />
+                            </IconButton>}
                     </Grid>
                     <Grid item>
                         <IconButton>
-                            <FavoriteBorderRoundedIcon/>
-                        </IconButton>   
+                            <Forward10RoundedIcon />
+                        </IconButton>
                     </Grid>
-                    </div>
-                </Grid>
-            </Card>
+                    <Grid item>
+                        <IconButton>
+                            <FavoriteBorderRoundedIcon />
+                        </IconButton>
+                    </Grid>
+                </div>
+            </Grid>
+        </Card>
     );
 }
 
