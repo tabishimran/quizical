336f60dd9e86762f66f593434dd590172f097879
Tabish Imran <tabish.imran96@gmail.com>
2021-05-13 21:27:42 +0530

    restructures on click function

--- packages/frontend/src/components/Question.tsx
+++ packages/frontend/src/components/Question.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction } from 'react';
+import React, { Dispatch, SetStateAction, useRef, useState } from 'react';
 import '../css/App.css';
 import QuizNav from './QuizNav';
 import Player from './Player';
@@ -15,13 +15,30 @@ function Question(props: questionProps) {
     var question = props.question;
     const nextQuestion = props.nextQuestion
     console.log(question);
+    const [answered,setAnswered] = useState(false);
+    
+    const getColor = function(){
+
+    }
+
+    const checkAnswer = function(each:string){
+        setAnswered(true)
+        if(each==question.answer){
+            console.log('true')
+        }
+        setTimeout(()=>{
+            setAnswered(false);
+            nextQuestion();
+        },1000);
+    }
+
     return (
         <Grid
             container
             direction="column"
             justify="center"
             alignItems="center"
-            style={{ textAlign: "center", height: '100%', marginTop: '4rem'}}
+            style={{ textAlign: "center", height: '100%', marginTop: '2rem'}}
         >
             <Grid item style={{ backgroundColor: "#191414" }}>
                 <Typography
@@ -40,16 +57,18 @@ function Question(props: questionProps) {
                 alignItems="center"
                 alignContent="center"
                 style={{
-                    marginTop: "4rem",
+                    marginTop: "3rem",
                     width: "100%"
                 }}
             >
                 {question.options.map((each) => (
                     <Grid item style={{ margin: "1rem", height: "100%", textAlign: "center" }}>
                         <Card
-                            onClick={nextQuestion}
+                            onClick={()=>{
+                                checkAnswer(each);
+                            }}
                             style={{
-                                backgroundColor: "#2f2929",
+                                backgroundColor: answered?each==question.answer?"#1DB954":"#2f2929":"#2f2929",
                                 height: "4rem",
                                 width: "13rem",
                                 textAlign:"center"
