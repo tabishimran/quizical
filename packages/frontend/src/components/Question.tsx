d7845a18c412cd867cd90f1a10d8a196a54711a8
Tabish Imran <tabish.imran96@gmail.com>
2021-05-13 18:09:50 +0530

    updates question component

--- packages/frontend/src/components/Question.tsx
+++ packages/frontend/src/components/Question.tsx
@@ -1,30 +1,71 @@
-import { Grid, Typography } from '@material-ui/core';
-import React from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
 import '../css/App.css';
+import QuizNav from './QuizNav';
+import Player from './Player';
+import { Card, CardActionArea, CardContent, Grid, Typography } from '@material-ui/core';
 
-function Question(){
-    const question="Which song is this›?";
-    return(
-        <div className="question">
-            <Grid 
+
+interface questionProps {
+    question: question,
+    nextQuestion: ()=>void
+}
+
+function Question(props: questionProps) {
+    
+    var question = props.question;
+    const nextQuestion = props.nextQuestion
+    console.log(question);
+    return (
+        <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+            style={{ textAlign: "center", height: '100%', marginTop: '4rem'}}
+        >
+            <Grid item style={{ backgroundColor: "#191414" }}>
+                <Typography
+                    variant="h4"
+                    style={{
+                        color: "white",
+                        fontSize: "1.5rem"
+                    }}>
+                    {question.question}
+                </Typography>
+            </Grid>
+            <Grid
                 container
                 direction="column"
+                justify="center"
+                alignItems="center"
+                alignContent="center"
+                style={{
+                    marginTop: "4rem",
+                    width: "100%"
+                }}
             >
-                <Grid item>
-                    <Typography 
-                        variant="body1"
-                        style={{
-                            color:"white",
-                            fontSize:"2vh"
-                        }}
-                    >
-                        {question}
-                    </Typography>
+                {question.options.map((each) => (
+                    <Grid item style={{ margin: "1rem", height: "100%", textAlign: "center" }}>
+                        <Card
+                            onClick={nextQuestion}
+                            style={{
+                                backgroundColor: "#2f2929",
+                                height: "4rem",
+                                width: "13rem",
+                                textAlign:"center"
+                            }}>
+                            <CardActionArea>
+                                <CardContent>
+                                    <Typography variant="body1" style={{ "color": "white" }}>
+                                        {each}
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+                ))};
                 </Grid>
-                <Grid item></Grid>
-                <Grid item></Grid>
-            </Grid>
-        </div>
+        </Grid>
     );
 }
 
