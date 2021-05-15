b71d27f190dfef2f2c4a5d2cd382de3bf988702e
Tabish Imran <tabish.imran96@gmail.com>
2021-05-16 01:20:07 +0530

    moves quiz view into grid

--- packages/frontend/src/views/Quiz.tsx
+++ packages/frontend/src/views/Quiz.tsx
@@ -10,16 +10,15 @@ import QuizNav from '../components/QuizNav';
 import Player from '../components/Player';
 import { useHistory } from 'react-router-dom';
 
-
 type quizParams = {
     artistUri: string
 }
 
-const initalQuizData={
-    question:"",
-    answer:"",
-    audio:"",
-    options:[""]
+const initalQuizData = {
+    question: "",
+    answer: "",
+    audio: "",
+    options: [""]
 }
 
 function Quiz() {
@@ -32,12 +31,12 @@ function Quiz() {
     const [questionNumber, setQuestionNumber] = useState(0);
     const [currentQuestion, setCurrentQuestion] = useState<question>(initalQuizData);
     const [totalQuestions, setTotalQuestions] = useState(0);
-    const [correctAnswers,setCorrectAnswer] = useState(0);
+    const [correctAnswers, setCorrectAnswer] = useState(0);
     const [songPlaying, setSongPlaying] = useState(false);
-    
+
     function nextQuestion() {
         if (questionNumber != quiz.length - 1) {
-            setCurrentQuestion(quiz[questionNumber+1]);
+            setCurrentQuestion(quiz[questionNumber + 1]);
             setQuestionNumber(questionNumber + 1);
         }
         else {
@@ -45,21 +44,21 @@ function Quiz() {
         }
     }
 
-    function incrementCorrectAnswers(){
-        setCorrectAnswer(correctAnswers+1);
+    function incrementCorrectAnswers() {
+        setCorrectAnswer(correctAnswers + 1);
     }
 
     function quizComplete() {
         const urlParams = new URLSearchParams();
-        urlParams.append('correctAnswers',correctAnswers.toString());
-        urlParams.append('totalQuestions',totalQuestions.toString());
-        history.push('/complete?'+urlParams.toString())
+        urlParams.append('correctAnswers', correctAnswers.toString());
+        urlParams.append('totalQuestions', totalQuestions.toString());
+        history.push('/complete?' + urlParams.toString())
     }
 
     useEffect(() => {
         async function getQuiz(artistUri: string) {
             const response = await fetch("https://quizical.tabishimran.com/api/quiz?artist=" + artistUri);
-            if(response.status==401) history.push('/login');
+            if (response.status == 401) history.push('/login');
             const data = await response.json()
             setTotalQuestions(data.length);
             setUpQuiz(data);
@@ -71,39 +70,53 @@ function Quiz() {
     }, []);
 
     const quizView =
-        <div style={{ backgroundColor: "#191414" }}>
+        <div style={{ backgroundColor: "#191414", height: "100vh" }}>
             <NavBar></NavBar>
-            <div className="quizNavigation">
-                <QuizNav questionNumber={questionNumber}></QuizNav>
-            </div>
-            <Question setSongPlaying={()=>{setSongPlaying(false)}} question={currentQuestion} nextQuestion={nextQuestion} incrementCorrectAnswers={incrementCorrectAnswers}></Question>
-            <div className="player" style={{
-                position: "relative",
-                bottom: "0",
-                width: "100%",
-                left: 0,
-                right: 0,
-                marginBottom: "2rem"
-            }}>
-                <Player songPlaying={songPlaying} setSongPlaying={setSongPlaying} audio={currentQuestion.audio}></Player>
-            </div>
+            <Grid
+                container
+                direction="row"
+                justify="center"
+                alignContent="center"
+                alignItems="center"
+                style={{ height: "100%", backgroundColor: "#191414" }}
+            >
+                <Grid item xs={12}>
+                    <div className="quizNavigation">
+                        <QuizNav questionNumber={questionNumber}></QuizNav>
+                    </div>
+                </Grid>
+                <Grid item xs={12}>
+                    <Question setSongPlaying={() => { setSongPlaying(false) }} question={currentQuestion} nextQuestion={nextQuestion} incrementCorrectAnswers={incrementCorrectAnswers}></Question>
+                </Grid>
+                <Grid item xs={12}>
+                    <div className="player" style={{
+                        position: "relative",
+                        bottom: "0",
+                        width: "100%",
+                        left: 0,
+                        right: 0,
+                    }}>
+                        <Player songPlaying={songPlaying} setSongPlaying={setSongPlaying} audio={currentQuestion.audio}></Player>
+                    </div>
+                </Grid>
+            </Grid>
         </div>
 
     const loadingScreen =
         <div className="container">
-                <Grid container
-                    direction="column"
-                    justify="center"
-                    alignItems="center"
-                    alignContent="center"
-                    style={{"height":"100%"}}
-                >
-                    <LoadingScreen></LoadingScreen>
-                </Grid>
+            <Grid container
+                direction="column"
+                justify="center"
+                alignItems="center"
+                alignContent="center"
+                style={{ "height": "100%" }}
+            >
+                <LoadingScreen></LoadingScreen>
+            </Grid>
         </div>
 
     return (
-        loading?loadingScreen:quizView
+        loading ? loadingScreen : quizView
     );
 
 }
